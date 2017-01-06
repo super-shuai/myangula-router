ad90e0e6fdca5e147ad2ce2fdada78096991e95a
YangXiaochun-admin <13098858665@qq.com>
2017-01-06 12:52:06 +0800

    angular

---
+++ test/services.js
@@ -0,0 +1,14 @@
+angular.module("services",[])
+.factory("indexDate",function($http){
+    //return [{title:"标题1",con:"内容"}]
+    return $http({url:"1.txt"})
+    //$http({url:"1.txt"}).then(function(data){
+    //    return data.data;
+    //})
+}).factory("listDate",function($http){
+    //return [{title:"标题1",con:"内容"}]
+    return $http({url:"list.txt"})
+    //$http({url:"1.txt"}).then(function(data){
+    //    return data.data;
+    //})
+})
